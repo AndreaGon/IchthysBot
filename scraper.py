ca9edf6ee6784f155442ac229da1f0ada3bee39e
Andrea Gonzales <AndreaGon@users.noreply.github.com>
2021-09-08 14:49:41 +0800

    Fix formatting for bible reading. Make it more readable and add necessary markdown

--- scraper.py
+++ scraper.py
@@ -21,38 +21,26 @@ class Ichthys:
 
         #Get the whole verse elements first, then extract unnecessary elements
         #Sometimes we will also extract necessary elements for formatting later
-        verse_content = soup.find('div', class_='passage-text')
-        chapter_header = verse_content.find("h3")
-        ignore_full_chapter = verse_content.find('a', class_="full-chap-link")
-        ignore_sup_tag = verse_content.find_all('sup', class_="footnote")
-        ignore_div_footnote = verse_content.find('div', class_="footnotes")
-        ignore_other_trans = verse_content.find('div', class_="passage-other-trans")
+        verse_content = soup.find('div', class_='version-RSVCE result-text-style-normal text-html')
 
-        if(ignore_div_footnote != None):
-            ignore_div_footnote.extract()
+        verses_format = []
+        for verse in verse_content.find_all():
+            if verse.name == 'h3':
+                verses_format.append("\n"+ "**" + verse.text + "**" + "\n\n")
+            elif verse.name == 'p':
+                ignore_sup_tag = verse.find('sup', class_="footnote")
+                if(ignore_sup_tag == None):
+                    verses_format.append(verse.text + "\n")
 
-        if(ignore_full_chapter != None):
-            ignore_full_chapter.extract()
-
-        if(ignore_other_trans != None):
-            ignore_other_trans.extract()
-
-        if(ignore_sup_tag != None):
-            for tag in range(len(ignore_sup_tag)):
-                ignore_sup_tag[tag].extract()
-
-        if(chapter_header != None):
-            chapter_header.extract()
-            chapter_header = chapter_header.text
-        else:
-            chapter_header = verse
-
-        formatted_verse = "**" + chapter_header + "**" + "\n" + verse_content.text + "Read from the website: " + "\n" + full_request
-
-        if len(formatted_verse) > 2000:
+        verses_format.append("\n\nRead from the website: \n" + full_request)
+        if len(verses_format) > 2000:
             error_message = "It looks like you're reading a Bible verse that has more than 2000 characters. Read instead from the website: \n" + full_request
             return error_message
-        return formatted_verse
+
+
+        return "".join(verses_format)
+
+
 
     def readPrayer(self, prayer):
         with open("prayers.json") as f:
